c74d7193c4284b63f6a8adcd03b6b1bdce3c498b
alirezaandersen <alireza.andersen@gmail.com>
2016-03-31 19:49:42 -0600

    first commit

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,21 @@
 class PartialArts < Sinatra::Base
+
+  helpers do
+
+    def artist_url
+    "<a href='/artists'>All Artists</a> <br>"
+    end
+
+    def link_to(route, title)
+      "<a href='/#{route}'>#{title}</a>"
+    end
+
+    def format_title
+      element.capitalize
+    end
+
+  end
+
   get '/artists' do
     @artists = Artist.all
     erb :'artists/index'
